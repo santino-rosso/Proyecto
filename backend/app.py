41a258ee5511f0729b27ee40b2eb574551028db1
marcos2504 <marcosibarra1234@gmail.com>
2023-04-04 11:04:56 -0300

    arreglado

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,5 @@ app = create_app()
 
 app.app_context().push()
 
-if  __name__ == '__main__' :
+if __name__ == '__main__': 
     app.run(debug=True,port=os.getenv('PORT'))
--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 #Módulo que inicializara la app y todos los modulos
 def create_app():
 #Inicio flask
-    app = Flask (__name__ )
+    app = Flask (__name__)
 
 #variables de entorno
     load_dotenv()
 
 #Iniciaremos los modulos de la app 
 #retornamos la app inicializada
-    return app.py
+    return app
