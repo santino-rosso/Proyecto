670314b2743a385f1a90a93bb4dfef1102a40182
marcos2504 <marcosibarra1234@gmail.com>
2023-10-19 01:30:26 -0300

    service register hecho

--- frontend/app_gym/src/app/pages/registrarse/registrarse.component.ts
+++ frontend/app_gym/src/app/pages/registrarse/registrarse.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrarse',
@@ -7,15 +9,46 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./registrarse.component.css']
 })
 export class RegistrarseComponent {
-  constructor(private authService: AuthService) {}
+  registerForm!: FormGroup;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    
+    ) {}
+    ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+        rol: ['', Validators.required],
+        dni: ['', Validators.required],
+        email: ['', Validators.required],
+        nombre: ['', Validators.required],
+        apellido: ['', Validators.required],
+        contraseña: ['', Validators.required],
+        telefono: ['', Validators.required]
 
-  onSubmit(formData: any) {
+      })
+     }
+
+  register(formData: any) {
     this.authService.register(formData).subscribe(
       (response) => {
+        alert("Registro exitoso");
+        this.router.navigate(['/login']);
       },
       (error) => {
+        alert("Registro fallido");
       }
     );
+  }
+
+
+  submit() {
+    if(this.registerForm.valid) {
+      console.log('Form login: ',this.registerForm.value);
+      this.register(this.registerForm.value)
+    } else {
+      alert('Formulario invalido');
+    }
   }
 
 }
